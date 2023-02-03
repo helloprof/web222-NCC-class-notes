b09db2a42ebd4bad3917956dc9d3595bf9543e69
helloprof <suluxan.seneca@gmail.com>
2023-02-03 17:08:20 -0500

    week 4 using a constructor

--- week4/objects.js
+++ week4/objects.js
@@ -27,3 +27,63 @@ var balance = {}
 balance.funds = -90.04
 balance.topUp = new Date()
 console.log(balance)
+balance.funds = 90
+console.log(balance)
+
+
+var senecaCard = {
+    studentID: 89008899,
+    studentName: "Suluxan Mohanraj",
+    studentBalance: -90.04,
+    transactions: {
+        2022: {
+            jan: [2.00, 1.00, 7.00],
+            feb: [5.00, 4.00, 5.00, 7.00]
+        }
+    },
+    getBalance: function() {
+        return this.studentBalance
+    },
+    updateBalance: function(change) {
+        if(change < 0) {
+            this.studentBalance - change
+        } else {
+            this.studentBalance + change 
+        }
+    }
+}
+
+senecaCard.getBalance()
+
+function StudentCardPrint(name, studentID) {
+    this.studentName = name
+    this.studentID = studentID
+    this.balance = 0 
+    this.transactions = {}
+    this.getBalance = function() {
+        return this.balance
+    }
+    this.updateBalance = function(change) {
+        if(change < 0) {
+            this.balance - change
+        } else {
+            this.balance + change 
+        }
+    }
+}
+
+// StudentCardPrint.prototype.getName = function() {
+//     return this.studentName
+// }
+
+
+
+var smStudentCard = new StudentCardPrint("Suluxan Mohanraj", 23899284)
+var jsStudentCard = new StudentCardPrint("Jon Snow", 23899284)
+
+
+console.log(smStudentCard.prototype.getName)
+// console.log(smStudentCard.getBalance())
+
+// class StudentCardClass...
+// var student = new StudentCardClass("Suluxan Mohanraj")
