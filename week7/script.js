0f9ed480287d6786f93d897420c1ea23ab610e41
helloprof <suluxan.seneca@gmail.com>
2023-03-06 13:15:15 -0500

    week7 dom work

---
+++ week7/script.js
@@ -0,0 +1,12 @@
+console.log("hello!")
+
+var redBox = document.querySelector("#bottom")
+
+redBox.addEventListener('click', function(e) {
+    // e.preventDefault()
+    // e.stopPropagation();
+    // var top = e.target
+    // top.style = "background-color:green"
+    console.log("hello")
+}) 
+
